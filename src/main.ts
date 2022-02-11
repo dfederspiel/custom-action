d0b282885a6bed8cbf933443c2e10daae767033e
David Federspiel <david@federnet.com>
2022-02-10 20:08:34 -0500

    feat: add github core

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,14 @@
 import * as core from '@actions/core'
+import * as github from '@actions/github'
 import {wait} from './wait'
 
 async function run(): Promise<void> {
   try {
     const ms: string = core.getInput('milliseconds')
     core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
-
+    core.debug(github.context.issue.repo)
     core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
+    await wait(parseInt(ms, 2000))
     core.debug(new Date().toTimeString())
 
     core.setOutput('time', new Date().toTimeString())
